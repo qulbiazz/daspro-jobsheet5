bc6c694b5b37722b5ae7c81e90dd5d918e3257f7
qulbyazz <qulbyazzumy@gmail.com>
2024-09-25 22:19:32 +0700

    commit percobaan 2

---
+++ PemilihanHariDenganIf22.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PemilihanHariDenganIf22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int angka;
+
+        System.out.print("Masukkan Angka: ");
+        angka = input.nextInt();
+
+        if (angka >= 1 && angka <= 5) {
+            System.out.println("Weekday");
+        }else if(angka >= 6 && angka <= 7){
+        System.out.println("Weekend");
+        }else{
+            System.out.println("Invalid Number");
+        }
+    }
+}
